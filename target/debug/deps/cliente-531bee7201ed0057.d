8507e804f7f202bac9c53796d745a7948084eb6b
Adrián Muñoz Alvarado <adrianmnzalv05@gmail.com>
2024-03-17 03:22:31 -0600

    App basica que muestra info y tiene boton de likes y dislikes, solo local

---
+++ target/debug/deps/cliente-531bee7201ed0057.d
@@ -0,0 +1,5 @@
+/home/erick/Documents/Tec/DatosII/Proyecto_I/cliente/target/debug/deps/libcliente-531bee7201ed0057.rmeta: src/main.rs
+
+/home/erick/Documents/Tec/DatosII/Proyecto_I/cliente/target/debug/deps/cliente-531bee7201ed0057.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/cliente-a923710e54e8ef0c.d
@@ -0,0 +1,5 @@
+/home/erick/Documents/Tec/DatosII/Proyecto_I/cliente/target/debug/deps/libcliente-a923710e54e8ef0c.rmeta: src/main.rs
+
+/home/erick/Documents/Tec/DatosII/Proyecto_I/cliente/target/debug/deps/cliente-a923710e54e8ef0c.d: src/main.rs
+
+src/main.rs:
